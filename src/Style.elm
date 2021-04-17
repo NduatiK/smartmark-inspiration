a51efebaa892347a2f5d9d20da9721b7c042412a
NduatiK <012nkuria@gmail.com>
2021-04-17 03:35:50 +0300

    Tweak fonts

--- src/Style.elm
+++ src/Style.elm
@@ -7,8 +7,8 @@ import Element.Font as Font
 monospace : Attribute msg
 monospace =
     Font.family
-        [ Font.typeface "Space Mono"
-        , Font.external { name = "Space Mono", url = "https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;1,700&display=swap" }
+        [ Font.typeface "Roboto Mono"
+        , Font.typeface "Space Mono"
         , Font.monospace
         ]
 
